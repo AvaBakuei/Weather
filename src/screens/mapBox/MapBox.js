659473583ced00e03a10c4a3bf041d3bc3ca5c90
Ava <avabakuie@gmail.com>
2019-10-31 13:18:29 +0330

    add icon manifest

--- src/screens/mapBox/MapBox.js
+++ src/screens/mapBox/MapBox.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactMapboxGl from "react-mapbox-gl";
+import ReactMapboxGl, { Marker } from "react-mapbox-gl";
 
 //local
 import "./MapBox.scss";
@@ -9,6 +9,9 @@ const MapBox = () => {
 		accessToken:
 			"pk.eyJ1IjoiaXRpbmFhYXIiLCJhIjoiY2syOHo3cHYwMHVkYTNjcGJ5N2w5djZmaSJ9.9leIJZc8mrrsdqtowI7IIg",
 	});
+	const handleGetLatLng = () => {
+		console.log("ff");
+	};
 	return (
 		<div>
 			<Map
@@ -18,7 +21,17 @@ const MapBox = () => {
 					height: `calc(100vh - 48px)`,
 					width: "100%",
 				}}
-				center={[51.389, 35.6892]}></Map>
+				center={[51.389, 35.6892]}>
+				<Marker
+					coordinates={[51.389, 35.6892]}
+					anchor="top"
+					onClick={handleGetLatLng}>
+					<img
+						src={require("../../assets/images/pin.png")}
+						alt="images"
+					/>
+				</Marker>
+			</Map>
 			;
 		</div>
 	);
